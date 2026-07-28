dff2e63680484190186660bccd3df7827956adf8
dimastadeoo <dimastadeoo@gmail.com>
2026-07-28 21:37:27 +0700

    feat: add success and error response utility functions

---
+++ backend/src/lib/response.js
@@ -0,0 +1,24 @@
+
+/**
+ * 
+ * @param {import("express").Response} res 
+ */
+export const successResponse = (res, message, data = null, statusCode = 200) => {
+  res.status(statusCode).json({
+    success: true,
+    message: message,
+    ...(data !== null && { results: data }),
+  })
+}
+
+/**
+ * 
+ * @param {import("express").Response} res 
+ */
+export const errorResponse = (res, message, statusCode = 500) => {
+  const response = {
+    success: false,
+    message: message,
+  }
+  res.status(statusCode).json(response)
+}
