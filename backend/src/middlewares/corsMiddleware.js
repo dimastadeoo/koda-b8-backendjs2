9f89835678f1c4c74368ea522eeb23714ba0d140
dimastadeoo <dimastadeoo@gmail.com>
2026-07-28 21:38:18 +0700

    feat: add CORS middleware to handle cross-origin requests

---
+++ backend/src/middlewares/corsMiddleware.js
@@ -0,0 +1,23 @@
+import { constants } from "node:http2"
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {function()} next
+ */
+function corsMiddleware(req, res, next) {
+    const PORT = process.env.PORT_FRONTEND || 5173
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:" + PORT)
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Origin, Authorization")
+
+    if (req.method === "OPTIONS") {
+        res.sendStatus(constants.HTTP_STATUS_NO_CONTENT)
+        return
+    }
+    next()
+}
+
+export default corsMiddleware
