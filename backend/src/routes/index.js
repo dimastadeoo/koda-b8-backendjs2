b1b77c9a08a92143260f9a004395978a83e51198
dimastadeoo <dimastadeoo@gmail.com>
2026-07-28 21:38:41 +0700

    feat: create main router to integrate authentication and notes routes

---
+++ backend/src/routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import authRoutes from './authRoutes.js'
+import noteRoutes from './notesRoutes.js'
+
+const router = Router()
+
+router.use('/auth', authRoutes)
+router.use('/notes', noteRoutes)
+
+export default router
