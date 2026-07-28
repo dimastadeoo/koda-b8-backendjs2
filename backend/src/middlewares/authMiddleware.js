0459986f1517245e7d9a79cade646499a7c5a61e
dimastadeoo <dimastadeoo@gmail.com>
2026-07-28 21:38:23 +0700

    feat: implement authentication middleware to verify user tokens

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+import { findByEmail } from '../models/usersModel.js'
+import * as Response from "../lib/response.js"
+import {constants} from "node:http2"
+
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ */
+export async function authenticate(req, res, next) {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return Response.errorResponse(res, 'Unauthorized: missing token', constants.HTTP_STATUS_UNAUTHORIZED)
+  }
+  const email = authHeader.split(' ')[1]
+  if (!email) {
+    return Response.errorResponse(res, 'Unauthorized: invalid token', constants.HTTP_STATUS_UNAUTHORIZED)
+  }
+
+  const user = await findByEmail(email)
+  if (!user) {
+    return Response.errorResponse(res, 'Unauthorized: user not found', constants.HTTP_STATUS_UNAUTHORIZED)
+  }
+
+  req.user = user
+  next()
+}
