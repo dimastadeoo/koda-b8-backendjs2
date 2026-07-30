3bc0421d357dd5c68ecd37560df3a174f930b83f
dimastadeoo <dimastadeoo@gmail.com>
2026-07-30 11:26:37 +0700

    fix: correct title in OpenAPI documentation for notes app

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,7 +8,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Hello Notes App Backend',
+      title: 'Notes App Backend',
       version: '1.0.0',
     },
     components: {
