408b4f3d5ea7f3478526f726b462cf3e5b36fb3b
dimastadeoo <dimastadeoo@gmail.com>
2026-07-30 14:06:55 +0700

    refactor: update authentication middleware to verify JWT token instead of email

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,28 +1,29 @@
-import { findByEmail } from '../models/usersModel.js';
 import * as Response from "../lib/response.js";
 import {constants} from "node:http2";
+import libJwt from '../lib/jwt.js';
 
 
 /**
  * 
  * @param {import("express").Request} req 
  * @param {import("express").Response} res 
+ * @param {function ()} next
  */
 export async function authenticate(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return Response.errorResponse(res, 'Unauthorized: missing token', constants.HTTP_STATUS_UNAUTHORIZED);
   }
-  const email = authHeader.split(' ')[1];
-  if (!email) {
-    return Response.errorResponse(res, 'Unauthorized: invalid token', constants.HTTP_STATUS_UNAUTHORIZED);
+  const token = authHeader.split(' ')[1];
+  if (!token) {
+    return Response.errorResponse(res, 'Unauthorized: token not found', constants.HTTP_STATUS_UNAUTHORIZED);
   }
 
-  const user = await findByEmail(email);
-  if (!user) {
-    return Response.errorResponse(res, 'Unauthorized: user not found', constants.HTTP_STATUS_UNAUTHORIZED);
+  const verifToken = libJwt.verify(token);
+  if (!verifToken) {
+    return Response.errorResponse(res, 'Unauthorized: Token invalid', constants.HTTP_STATUS_UNAUTHORIZED);
   }
 
-  req.user = user;
+  req.user = verifToken;
   next();
 }
