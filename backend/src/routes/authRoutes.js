9c73280b54a5c0eaa410390ee09b4ae36a10e9f7
dimastadeoo <dimastadeoo@gmail.com>
2026-07-30 11:19:16 +0700

    feat: add OpenAPI documentation for register and login routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,67 @@ import { register, login } from '../controllers/authController.js';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /auth/register:
+ *   post:
+ *     tags:
+ *      - Auth
+ *     description: Register New User!
+ *     requestBody:
+ *       description: Input Form to add New User
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             required:
+ *              - name
+ *              - email
+ *              - password
+ *             type: object
+ *             properties:
+ *              name:
+ *               type: string
+ *              email:
+ *               type: string
+ *               format: email
+ *              password:
+ *               type: string
+ *               format: password
+ *       required: true
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.post('/register', register);
+
+/**
+ * @openapi
+ * /auth/login:
+ *   post:
+ *     tags:
+ *      - Auth
+ *     description: Login User!
+ *     requestBody:
+ *       description: Input form for login
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             required:
+ *              - email
+ *              - password
+ *             type: object
+ *             properties:
+ *              email:
+ *               type: string
+ *               format: email
+ *              password:
+ *               type: string
+ *               format: password
+ *       required: true
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.post('/login', login);
 
 export default router;
