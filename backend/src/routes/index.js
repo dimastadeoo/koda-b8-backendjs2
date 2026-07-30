0337c490a531c406e02b5f32a430b0d38ae9aec2
dimastadeoo <dimastadeoo@gmail.com>
2026-07-30 11:19:09 +0700

    feat: add Swagger documentation setup for API routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,35 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes.js';
 import noteRoutes from './notesRoutes.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello Notes App Backend',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        token: {
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
+      }
+    },
+  },
+    
+  apis: ['./src/routes/*.js'],
+};
 
 const router = Router();
+
+const jsdocSetup = swaggerJSDoc(options);
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(jsdocSetup));
 
 router.use('/auth', authRoutes);
 router.use('/notes', noteRoutes);
